6e16ebaec84507434966d51affef87fb276b30f8
MdReyadHossain <reyadhosen@gmail.com>
2024-11-09 01:15:29 +0600

    solved

---
+++ Easy/342. Power of Four.cs
@@ -0,0 +1,13 @@
+public class Solution
+{
+    public bool IsPowerOfFour(int n)
+    {
+        if (n <= 0)
+            return false;
+        while (n % 4 == 0)
+        {
+            n /= 4;
+        }
+        return n == 1;
+    }
+}
